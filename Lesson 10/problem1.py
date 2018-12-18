90f4ea790a1af81f3e375621271a822beae5a383
fpirekso <43756366+fpirekso@users.noreply.github.com>
2018-12-18 12:35:22 -0500

    Add files via upload

---
+++ Lesson 10/problem1.py
@@ -0,0 +1,8 @@
+print('_'*70)
+print('Life Decision Bot:')
+hw = input ('Did you do your homework? ')
+if hw == 'yes':
+	print('Cool! Go outside I guess or watch tv')
+else:
+	print('Do your homework! Why are you so lazy?')
+print('_'*70)
